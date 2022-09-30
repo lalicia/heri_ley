96f487a222f4d7f39d5645fbec7c7ff8f0ba6568
alicialowe <hohpascal@outlook.com>
2022-09-30 09:29:45 +0100

    few small tweaks to Navbar styling and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,10 +14,10 @@ function Navbar() {
             </div>
 
             <div className="navlinks">
-                <h3><a href="#about">ABOUT</a></h3>
-                <h3><a href="#about">EVENTS</a></h3>
+                <h3><a className="navlink" href="#about">ABOUT</a></h3>
+                <h3><a className="navlink" href="#events">EVENTS</a></h3>
                 {/* <div className="socialslink"> */}
-                    <a href="https://www.instagram.com/ragnavladez/"><img className="socialslogo" src={Instagram} alt="Instagram" /></a>
+                    <a className="navlink" href="https://www.instagram.com/ragnavladez/" target="_blank" rel="noreferrer"><img className="socialslogo" src={Instagram} alt="Instagram" /></a>
                 {/* </div> */}
             </div>
         </nav>
